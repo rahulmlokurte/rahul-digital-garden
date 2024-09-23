60137a42b268bccfc9cf9be5864b0f83cbbe48e1
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-09-23 18:11:24 +0530

    Fix the link

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "rahulsecondbrain.com",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
